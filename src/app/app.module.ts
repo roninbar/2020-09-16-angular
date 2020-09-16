84bc2910a8867b8eb0b53d871f3951b5a2f7c0ad
ron <rinbar70@gmail.com>
2020-09-16 18:52:11 +0300

    Take changes from Ofer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ACompComponent } from './a-comp/a-comp.component';
+import { BCompComponent } from './b-comp/b-comp.component';
 
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent
+        ACompComponent,
+        BCompComponent,
+        NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+        RouterModule.forRoot([
+            { path: 'a', component: ACompComponent },
+            { path: 'b/:name', component: BCompComponent },
+            { path: 'b', component: BCompComponent },
+            { path: '**', component: NotFoundComponent }
+        ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
